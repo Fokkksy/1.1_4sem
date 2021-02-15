ea16a9035ddeab556ec21cef24fb608791a4dd69
Vadim <nikitin.vl@phystech.edu>
2021-02-15 13:29:04 +0300

    Добвлен вывод результата

--- main.cpp
+++ main.cpp
@@ -23,6 +23,11 @@ int main() {
 		cout << "Sorry "<< another_number << " mathes previous number " << number << endl;
 		cin >> number;
 	}
+
+	cout << "The sum of inputed numers is " << number + another_number << endl;
+	cout << "The differance of inputed numers is " << number - another_number << endl;
+	cout << "The multiplication of inputed numers is " << number * another_number << endl;
+	cout << "The divion of inputed numers is " << another_number / number << endl;
 	
 	return 0;
 }
