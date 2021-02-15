7f6905c6ec7a1023ecac4e316b8e4e87441226c2
Vadim <nikitin.vl@phystech.edu>
2021-02-15 12:50:59 +0300

    Добалена просьба загадать число

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main() {
-	cout << "The programm works OK" << endl;
+	int number;
+	cout << "Think number... and input it here " << endl;
+	cin >> number;
+
 	return 0;
 }
